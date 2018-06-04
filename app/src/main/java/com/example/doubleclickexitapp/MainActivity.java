8a3301878256fc403ca1c52c75ab427382ad646d
HelloGuoYing <38399776+HelloGuoYing@users.noreply.github.com>
2018-06-04 16:33:48 +0800

    双击返回退出应用程序： 第一次点击Toast“再点击一次退出应用程序” 间隔2秒失效，再次点击退出应用程序

---
+++ app/src/main/java/com/example/doubleclickexitapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.doubleclickexitapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //记录用户首次点击返回键的时间
+    private long firstTime = 0;
+
+    /**
+     * 第一种解决办法 通过监听keyUp
+     * @param keyCode
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_UP) {
+            long secondTime = System.currentTimeMillis();
+            if (secondTime - firstTime > 2000) {
+                Toast.makeText(MainActivity.this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                firstTime = secondTime;
+                return true;
+            } else {
+                System.exit(0);
+            }
+        }
+
+        return super.onKeyUp(keyCode, event);
+    }
+}
